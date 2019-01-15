8db7445cc9eada3c3c00203547873d71494ee092
Max G <maximusg@gmail.com>
2019-01-15 13:22:23 -0800

    added Sweet ship skin, music and a sound effects, also hooked controls up to the mouse

--- GUI.py
+++ GUI.py
@@ -5,14 +5,68 @@ import sys
 import pygame
 
 pygame.init()
+pygame.mixer.init() #alllows for sounds
 
-WINDOWSIZE = (800, 600)
+WINDOWSIZE = (800, 600) # bottom right corner of window is posistion 800X600
 
 SCREEN = pygame.display.set_mode(WINDOWSIZE)
 
+#working with Text
+FONT = pygame.font.SysFont("Myriad Pro",48)
+HELLO = FONT.render("Hello World",1, (255,0,255), (255,255,255))
+HELLOsize = HELLO.get_size()
+
+#loading an image
+image = pygame.image.load("SweetShip.png")
+imageSize = image.get_size()
+
+#for sound
+sound = pygame.mixer.Sound("explosn.wav") #needs to be .wavfile I think
+pygame.mixer.music.load("roboCop3NES.mp3")
+
+x,y =0,100
+directionX =1
+directionY =1
+clock = pygame.time.Clock()
+pygame.mouse.set_visible(0) #removes the visibility of the mouse
+
+pygame.mixer.music.play(1,0.0)#starts the music
+
 while 1:
+    clock.tick(300)#runs # of times per second. More times means faster
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+    SCREEN.fill((0,0,0))
+    #SCREEN.blit(HELLO,(x,y))
+    SCREEN.blit(image,(x,y))
+    
+    #working with Mouse:
+    mousePosistion = pygame.mouse.get_pos()
+    x,y = mousePosistion
+    if x +imageSize[0]>800 :
+        x=800-imageSize[0]
+        sound.stop()
+        pygame.mixer.Channel(0).play(sound, maxtime=600)# allows to play over top of music in background
+    if y +HELLOsize[1]>600 :
+        y=600-imageSize[1]
+        sound.stop()
+        pygame.mixer.Channel(0).play(sound, maxtime=600)
+    if x==0 or y==0:
+        sound.stop()
+        pygame.mixer.Channel(0).play(sound, maxtime=600)
 
+    
+
+    #Bouncing Hellow world
+    # x +=directionX
+    # y += directionY
+    # if x + HELLOsize[0]>800 or x<=0:
+    #     directionX *=-1
+    # if y + HELLOsize[1]>600 or y<=0:
+    #     directionY *=-1
+        
+    
+   
     pygame.display.update()
