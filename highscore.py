01f4a64451b35fd92fa030f268312273da893e5a
Christopher Norine <chris.norine@gmail.com>
2019-01-31 11:53:11 -0800

    gui_work. some cleanup

--- highscore.py
+++ highscore.py
@@ -1,89 +1,61 @@
-#!/bin/python
-
-import pathlib
-
-def sanitizeName(value):
-    if len(value) > 60:
-        return value[:60]
-    return value
-
-def checkNumerical(value):
-    if value == None or not isinstance(value, int):
-        raise RuntimeError(value + 'is not a valid input.')
-    return value
-
-class scoreboard(object):
-
-    class score_entry(object):
+class Scoreboard(object):
+    class Entry(object):
         def __init__(self, name, score):
-            self.score = score
             self.name = name
+            self.score = score
             self.nextEntry = None
-            self.prevEntry = None
         
-        @property
-        def score(self):
-            return self.__score
-
         @property
         def name(self):
             return self.__name
 
         @property
+        def score(self):
+            return self.__score
+
+        @property
         def nextEntry(self):
             return self.__nextEntry
 
-        @score.setter
-        def score(self, value):
-            self.__score = checkNumerical(value)
-        
         @name.setter
-        def name(self, value):
-            self.__name = sanitizeInput(value)
+        def name(self, nameIn):
+            if not isinstance(nameIn, str):
+                raise RuntimeError(nameIn + 'is not a valid name for score entry')
+            self.__name = nameIn
         
-        @nextEntry.setter
-        def nextEntry(self, value):
-            if not None or not isinstance(value, self.__class__):
-                raise RuntimeErorr(value + 'is not a valid scoreboard entry')
-            self.__nextEntry = value
+        @score.setter
+        def score(self, scoreIn):
+            if not isinstance(scoreIn, int):
+                raise RuntimeError(scoreIn + 'is not a valid score for score entry')
+            self.__score = scoreIn
 
+        @nextEntry.setter
+        def nextEntry(self, entryIn):
+            if not (isinstance(entryIn, self.__class__) or entryIn == None):
+                raise RuntimeError(entryIn + 'is not a valid assignment for nextEntry')
+            self.__nextEntry = entryIn
+        
     def __init__(self):
         self.head = None
         self.tail = None
-        pass
-    
-    def add(self, value):
-        if not isinstance(value, self.score_entry):
-            raise RuntimeError(value + 'is not a valid scoreboard entry')
-        if not self.head:
-            self.head = value
-            self.tail = value
-        else:
-            while currEntry.score > value.score and currEntry.nextEntry != None:
-                currEntry = currEntry.nextEntry
-            value.prevEntry = currEntry.prevEntry
-            value.nextEntry = currEntry
-            value.prevEntry.nextEntry = value
-            currEntry.prevEntry = value
+        self.__length = 0
 
-    def getOnList(self, value):
-        if value <= self.tail.value:
-            return False
-        return True
+    def add(self, entry):
+        currEntry = self.head
+        while currEntry.score > entry.score:
+            pass
 
-    def reset(self):
+    def resetList(self):
         self.head = None
         self.tail = None
+        self.__length = 0
 
-    def __str__(self):
-        currEntry = self.head
-        result = ''
-        result += '***HALL OF FAME!!!***\n'
-        if currEntry != None:
-            while currEntry.nextEntry:
-                result += currEntry.name + ',' + str(currEntry.score)
-        return result
+    def belongsOnList(self, score):
+        return score > self.tail.score
+
+    def writeToFile(self):
+        pass
+
+    def readFromFile(self):
+        pass
         
-if __name__ == '__main__':
-    high_score_list = scoreboard()
-    print(high_score_list)
