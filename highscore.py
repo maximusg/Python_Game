878e05cbe7aa86f4f3fd5ed340fb0c7709523e35
Christopher Norine <christopher.norine@nps.edu>
2019-01-31 21:24:20 -0800

    First working implementation of a scoreboard.  Functional, but not full of best practice goodness yet.

--- highscore.py
+++ highscore.py
@@ -34,6 +34,9 @@ class Scoreboard(object):
             if not (isinstance(entryIn, self.__class__) or entryIn == None):
                 raise RuntimeError(entryIn + 'is not a valid assignment for nextEntry')
             self.__nextEntry = entryIn
+
+        def __str__(self):
+            return self.name + '  |  ' + str(self.score)
         
     def __init__(self):
         self.head = None
@@ -41,9 +44,20 @@ class Scoreboard(object):
         self.__length = 0
 
     def add(self, entry):
-        currEntry = self.head
-        while currEntry.score > entry.score:
-            pass
+        if self.head == None:
+            self.head = entry
+            self.tail = entry
+
+        if self.head.score < entry.score:
+            entry.nextEntry = self.head
+            self.head = entry
+        else:
+            currEntry = self.head
+            while currEntry.nextEntry != None and currEntry.nextEntry.score >= entry.score:
+                currEntry = currEntry.nextEntry
+            entry.nextEntry = currEntry.nextEntry
+            currEntry.nextEntry = entry
+        self.__trim()
 
     def resetList(self):
         self.head = None
@@ -54,8 +68,54 @@ class Scoreboard(object):
         return score > self.tail.score
 
     def writeToFile(self):
-        pass
+        fileName = open('highscores.asset', 'w')
+        currEntry = self.head
+        while currEntry.nextEntry:
+            if currEntry.nextEntry.nextEntry != None:
+                fileName.write(currEntry.name+'.'+str(currEntry.score)+',')
+            else:
+                fileName.write(currEntry.name+'.'+str(currEntry.score))
+            currEntry = currEntry.nextEntry
+        fileName.close()
 
-    def readFromFile(self):
-        pass
+    def readFromFile(self, fileName):
+        with open(fileName) as f:
+            read_data = f.read().split(',')
+        for entry in read_data:
+            temp = entry.split('.')
+            self.add(self.Entry(temp[0],int(temp[1])))
+
+
+    def __trim(self):
+        currEntry = self.head
+        i = 0
+        while currEntry.nextEntry != None and i < 20:
+            currEntry = currEntry.nextEntry
+            i += 1
+        self.tail = currEntry
+        self.tail.nextEntry = None 
+
+    def __str__(self):
+        result = '***HALL OF FAME!***\n'
+        currEntry = self.head
+        i = 1
+        result += str(i) + ') ' + str(currEntry) + '\n'
+        i += 1
+        while currEntry.nextEntry:
+            result += str(i) + ') ' + str(currEntry) + '\n'
+            i += 1
+            currEntry = currEntry.nextEntry
+        return result
         
+if __name__ == '__main__':
+    #topscores = Scoreboard()
+    #for i in range(21):
+    #    score = Scoreboard.Entry('CRN', 10000)
+    #    topscores.add(score)
+    #print(topscores)
+    #topscores.writeToFile()
+    topscore2 = Scoreboard()
+    topscore2.readFromFile('highscores.asset')
+    print(topscore2)
+
+    
