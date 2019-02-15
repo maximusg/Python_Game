e3ce911c2e7b2a93032802f9c8e361bd1463aafd
Max Geiszler <maximusg@gmail.com>
2019-02-15 14:16:06 -0800

    working with movement, NOT WORKING YET

--- movement.py
+++ movement.py
@@ -8,15 +8,28 @@ Ideas: go down, left, right, up, circle, spiral, slow start then speed up, up an
 
 
 class Move(object):
-    def __init__(self, rect_to_move, angle, speed, move_limit = 30):
+    def __init__(self, rect_to_move, behaviorArray, moveCountArray, speedArray=10, exitscreen=True):
 
-        #self.rect.centerx, self.rect.top = origin_x, origin_y
+        behaveDic = {"dive": self.__dive__(), "up":self.__up__(), 
+        "left":self.__left__(), "right":self.__right__()}
+ 
+        self.beaviors = behaviorArray #list of things to run from the behaveDic
+        self.moveCounts = moveCountArray# array frame amounts to do move count
+        self.speeds = speedArray #used to change speeds between behaviors, if no more speeds left, defaults to last speed given
+        self.speed = self.latestSpeed() #method that will update the current speed from self.speeds,
+        self.exitsceen = exitscreen #if true, this behaivor will result in enemy off screen and gone
+        
+        #execute default behaviors
+
+        
+        #self.rect.centerx, self.rect.top origin_x, origin_y
 
         #location
         #self.origin_x = origin_x
         #self.origin_y = origin_y
 
         self.rect = rect_to_move
+        self.rect
 
         #angle and speed
         self.angle = angle
