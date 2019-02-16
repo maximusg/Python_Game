086d399bad4dc564fbe8c59f3b243faa97f319c7
Max Geiszler <maximusg@gmail.com>
2019-02-15 16:29:01 -0800

    updated enemy movements, made three classes of behaivor NOT tested

--- enemy.py
+++ enemy.py
@@ -1,17 +1,41 @@
 import entity2
+import movement
 from library import *
 
 
 
 class enemy(entity2.entity2):
-	def __init__(self, weapon, imgFile):
+	def __init__(self, origin_x, origin_y, imgFile="enemy.png", speed=1, behavior="dive", weapon="spitfire", health=1):
 		super().__init__()
 		self.weapon = weapon
 		self.point_value = 500
 		self.image, self.rect = load_image(imgFile)
-		self.rect.centerx, self.rect.top = 300, 50
-		self.area = pygame.Rect(COLUMN_WIDTH, 0, SCREEN_WIDTH-(2*COLUMN_WIDTH), SCREEN_HEIGHT)
-		self.speed = 4
+		#self.rect.centerx, self.rect.top = 300, 50
+		# self.area = pygame.Rect(COLUMN_WIDTH, 0, SCREEN_WIDTH-(2*COLUMN_WIDTH), SCREEN_HEIGHT)
+		self.speed = speed #this will be a scaler for movement type
+		self.health = health
+		self.rect.x = origin_x
+		self.rect.y = origin_y
+
+		self.behaveDic = {"diver":__diver__(), 
+		"camper":__camper__(), "sleeper":__sleeper__()}
+		self.movement = self.behaveDic[behavior]
+
+	def __diver__(self):
+		return movement.Move(speedArray=[10*self.speed]) #default behavior for object, could increase/decrease speed
+	
+	def __camper__(self):#stays on the screen camping, will progress down the screen slowly doing squares
+		behaviorArray = ["down","stop","left","stop","up","stop","right"]
+		moveCountArray = [80,	100,	40,		100,  50,	100,	40]
+		speedArray = [	  25*self.speed,	5*self.speed]
+		return movement.Move(behaviorArray,moveCountArray,speedArray,False)
+	
+
+	def __sleeper__(self):# moves down and stops for a LLLONG time =, the wiggles left and right FAST, the stops again, the dives down
+		behaviorArray = ["down","stop","left","right","stop","down","right"]
+		moveCountArray = [80,	600,	40,		40,  	600,	800]
+		speedArray = [	  5*self.speed,	25*self.speed]
+		return movement.Move(behaviorArray,moveCountArray,speedArray)
 	
 	def move(self, x, y):
 		if self.rect.left < self.area.left: ###I hate this function. I need to make it better. -Chris
@@ -25,4 +49,4 @@ class enemy(entity2.entity2):
 		self.dirty = 1
 
 	def update(self):
-		self.move(self.speed, 0)
+		self=self.movement.update(self)
