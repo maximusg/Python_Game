700bbe7b96714156376373009fe10c87f4684d42
greg-norton <greg.norton93@gmail.com>
2019-02-19 21:41:49 -0800

    picking up powerups actually does something.
    weapon upgrade progress managed within weapon via the upgrade utility function
    
    Added some logic to GUI2 for upgrading items if the item is a weapon powerup

--- item_pickup.py
+++ item_pickup.py
@@ -7,8 +7,9 @@ cwd = Path.cwd()
 item_images_path = cwd.joinpath('resources', 'item_images')
 
 master_items_dict = {
-    'powerup':('powerup.gif'),
-    'coin':('coin.png')
+    'powerup':('powerup.gif', None),
+    'coin':('coin.png', None),
+    'spitfire_powerup':('spitfire_powerup.png', 'spitfire')
 }
 
 
@@ -16,14 +17,21 @@ class item(entity2.entity2):
     def __init__(self, origin_x, origin_y, speed = 1, path_to_img = 'powerup.gif', name = None):
         super().__init__()
 
+        self.weapon_name = None
         if name is not None:
             if name in master_items_dict:
-                path_to_img = master_items_dict.get(name)
+                path_to_img = master_items_dict.get(name)[0]
+                weapon_name = master_items_dict.get(name)[1]
+                self.is_weapon = weapon.is_weapon(weapon_name)
+                if self.is_weapon:
+                    self.weapon_name = self.is_weapon
+
         self.image, self.rect = load_image(item_images_path.joinpath(path_to_img))
         self.image = self.image.convert()
         self.rect.centerx, self.rect.top = origin_x, origin_y
         self.dirty = 1
-        self.is_weapon = weapon.is_weapon(name)
+
+
         self.name = name
 
 
