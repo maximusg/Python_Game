51a2284708d5e1c79b6543866b45b8a699d45349
Christopher Norine <chris.norine@gmail.com>
2019-02-26 11:05:37 -0800

    now contains MOAR EXPLOSIONS

--- explosion.py
+++ explosion.py
@@ -8,12 +8,18 @@ class ExplosionSprite(pygame.sprite.Sprite):
         self.rect = pygame.Rect(startx,starty,30,30)
         self.sound = load_sound('explosion.ogg')
         self.frame_counter = 45
+        self.frame = 1
         self.visible = 1
 
     def update(self):
         if self.frame_counter == 0:
             self.visible = 0
         self.frame_counter -= 1
+        if self.frame_counter % 5 == 0 and self.frame < 7:
+            self.frame += 1
+            old_x, old_y = self.rect.x, self.rect.y
+            self.image, self.rect = load_image('resources/misc_sprites/explosion'+str(self.frame)+'.png')
+            self.rect.x, self.rect.y = old_x, old_y
         self.move(0,5)
 
     def move(self, diffx, diffy):
